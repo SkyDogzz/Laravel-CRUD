c325e8cf042962264caa9ac3e86023b446ad368b
skydogzz <thomas.stephan@live.fr>
2022-05-18 05:12:22 +0200

    Setup seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,7 +19,7 @@ class PostFactory extends Factory
         return [
             'title' => $this->faker->sentence,
             'content' => $this->faker->paragraph,
-            'user_id' => random_int(1, 11),
+            'user_id' => random_int(1, 10),
         ];
     }
 }
